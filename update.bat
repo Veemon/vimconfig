bee8f81e8352e5eb0772f86c8d6b61046ffaddf0
Veemon <russelhughdemos@gmail.com>
2022-11-04 03:03:28 +1100

    update the update

--- update.bat
+++ update.bat
@@ -2,22 +2,36 @@
 setlocal
 
 set vimrc_path="C:\Program Files (x86)\Vim\_vimrc"
-set colorscheme_path="C:\Program Files (x86)\Vim\vimfiles\colors\shantyv2.vim"
+set vim_colors_path="C:\Program Files (x86)\Vim\vimfiles\colors\shantyv2.vim"
+set lightline_colors_path="C:\Program Files (x86)\Vim\vimfiles\bundle\lightline.vim\autoload\lightline\colorscheme\shantyline.vim"
 
-echo | set /p dummy="[1/2] checking vimrc       --  " 
-diff ./_vimrc %vimrc_path% > nul
+set vimrc_src="./_vimrc"
+set vim_colors_src="./colors/shantyv2.vim"
+set lightline_colors_src="./colorscheme/shantyline.vim"
+
+echo | set /p dummy="[1/3] checking vimrc" 
+diff %vimrc_src% %vimrc_path% > nul
 if "%errorlevel%" == "1" (
     echo updated
-    cp %vimrc_path% .\_vimrc
+    cp %vimrc_path% %vimrc_src%
 )
 echo.
 
-echo | set /p dummy="[2/2] checking colorscheme --  " 
-diff ./colors/shanty.vim %colorscheme_path% > nul
+echo | set /p dummy="[2/3] checking vim colors" 
+diff %vim_colors_src% %vim_colors_path% > nul
 if "%errorlevel%" == "1" (
     echo updated
-    cp %colorscheme_path% .\colors\shantyv2.vim
+    cp %vim_colors_path% %vim_colors_src%
 )
+echo.
+
+echo | set /p dummy="[3/3] checking lightline colors" 
+diff %lightline_colors_src% %lightline_colors_path% > nul
+if "%errorlevel%" == "1" (
+    echo updated
+    cp %lightline_colors_path% %lightline_colors_src%
+)
+echo.
 echo.
 
 git status
